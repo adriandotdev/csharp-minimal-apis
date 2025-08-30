407cb42c12e6b956e5234cef13d9cc4282b5f17a
Adrian Nads Marcelo <marcelonads@gmail.com>
2025-08-30 16:33:51 +0800

    add update category api

--- Endpoints/CategoryEndpoint.cs
+++ Endpoints/CategoryEndpoint.cs
@@ -8,25 +8,61 @@ public static class CategoryEndpoints
 
         var mapGroup = app.MapGroup("/api/v1/categories");
 
-        mapGroup.MapPost("/", async (Category category, AppDbContext db) =>
+        mapGroup.MapPost("/", CreateCategory);
+
+        mapGroup.MapGet("/{id}", GetCategoryById);
+
+        mapGroup.MapGet("/", GetCategories);
+
+        mapGroup.MapPut("/{id}", UpdateCategoryById);
+
+    }
+
+    private static async Task<IResult> CreateCategory(Category category, AppDbContext db)
+    {
+        db.Categories.Add(category);
+
+        await db.SaveChangesAsync();
+
+        return TypedResults.Created($"/api/v1/categories/{category.Id}", category);
+    }
+
+    private static async Task<IResult> GetCategories(AppDbContext db)
+    {
+        return TypedResults.Ok(await db.Categories.ToListAsync());
+    }
+
+    private static async Task<IResult> GetCategoryById(int id, AppDbContext db)
+    {
+        var category = await db.Categories.FindAsync(id);
+
+        if (category is null) return TypedResults.NotFound(new
         {
-            db.Categories.Add(category);
-
-            await db.SaveChangesAsync();
-
-            return Results.Created($"/api/v1/categories/{category.Id}", category);
+            Message = $"Product with ID of {id} is not found"
         });
 
-        mapGroup.MapGet("/{id}", async (int id, AppDbContext db) =>
-        {
-            var category = await db.Categories.FindAsync(id);
+        return TypedResults.Ok(category);
+    }
 
-            return Results.Ok(category);
+    private static async Task<IResult> UpdateCategoryById(int id, Category category, AppDbContext db) {
+
+        var categoryToUpdate = await db.Categories.FindAsync(id);
+
+        if (categoryToUpdate is null) return TypedResults.NotFound(new
+        {
+            Message = $"Category with ID of {id} is not found"
         });
 
-        mapGroup.MapGet("/", async (AppDbContext db) =>
+        categoryToUpdate.Name = category.Name ?? categoryToUpdate.Name;
+        categoryToUpdate.Description = category.Description ?? categoryToUpdate.Description;
+
+        await db.SaveChangesAsync();
+
+        return TypedResults.Ok(new
         {
-            return await db.Categories.ToListAsync();
+
+            Message = "Category updated successfully",
+            category = categoryToUpdate
         });
     }
 }
